d0036cf02a9940221b333731b9821d2d1458874f
CodingBuddy098 <codingbuddy098@gmail.com>
2025-04-21 16:51:20 +0530

    Add files via upload

---
+++ datacleaning.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv("netflix_titles.csv")
+
+
+print("Missing values before cleaning:")
+print(df.isnull().sum())
+
+print("\nDuplicate rows before cleaning:", df.duplicated().sum())
+df['director'] = df['director'].str.strip()
+df['country'] = df['country'].str.strip()
+df['cast'] = df['cast'].str.strip()
+df['cast'] = df['cast'].str.replace(' ', ' ')
+print("\nCurrent data types:")
+print(df.dtypes)
+df['show_id'] = df['show_id'].astype(str)
+print("\nDuplicate rows after cleaning:", df.duplicated().sum())
+df.to_csv('netflix_titles.csv',index=False)
