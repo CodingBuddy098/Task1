e5574b23d698387b22b4d5c8a7c18500436939c3
CodingBuddy098 <codingbuddy098@gmail.com>
2025-04-21 16:53:21 +0530

    Update datacleaning.py

--- datacleaning.py
+++ datacleaning.py
@@ -2,10 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("netflix_titles.csv")
 
-
 print("Missing values before cleaning:")
 print(df.isnull().sum())
-
 print("\nDuplicate rows before cleaning:", df.duplicated().sum())
 df['director'] = df['director'].str.strip()
 df['country'] = df['country'].str.strip()
